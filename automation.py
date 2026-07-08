9d548e1bd7f941e40feb1496160f8b6bea9257d6
Natan <natanssurek@hotmail.com>
2026-07-08 15:39:05 -0300

    Ajeitando o email e a senha

--- automation.py
+++ automation.py
@@ -18,9 +18,12 @@ time.sleep(5)
 
 #Passo 4: Fazer login
 pyautogui.click(x=907, y=497)
-pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.write("administrador@gmail.com")
 pyautogui.press("Tab")
-pyautogui.write("sua senha muito muito dificilima") 
+pyautogui.write("SenhaDoADM12345") 
+pyautogui.press("Tab")
+pyautogui.press("Enter")
+
 #Passo 5: Cadastrar 1 produto 
 #Passo 6: Repetir o passo 5 até acabar os produtos que precisam ser registrados
 
